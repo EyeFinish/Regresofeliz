593a9289ee439679edda6db786b9747304829f6f
Benjamin Riveros <benjamin4riveros@gmail.com>
2026-03-13 11:21:33 -0300

    Reorganizar estructura del proyecto: imagenes a assets/, docs centralizados, eliminar archivos redundantes

--- js/config.example.js
+++
@@ -1,7 +0,0 @@
-// CONFIGURACIÓN DE API KEYS - PLANTILLA
-// Copia este archivo como config.js y reemplaza con tus propias claves
-
-const CONFIG = {
-    // Token de Mapbox - Obtén el tuyo en https://account.mapbox.com/access-tokens/
-    MAPBOX_TOKEN: 'TU_TOKEN_DE_MAPBOX_AQUI'
-};
